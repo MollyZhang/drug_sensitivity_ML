ad2b0270f60344b8c884f78dbca3f0aad706b2a8
Molly Zhang <mollyzhang@ucsc.edu>
2016-09-28 14:41:39 -0700

    add function to generate cell predicate data for psl model

--- psl/generate_data.py
+++ psl/generate_data.py
@@ -4,15 +4,16 @@ import pandas as pd
 import numpy as np
 
 
-DATA_FOLDER = "data/first_model/"
 DRUG_TARGET_RAW = "../data/combined_annotations_CCLEmapped.tab"
+ESSEN_RAW = "../data/Achilles_QC_v2.4.3.rnai.Gs.gct"
+MRNA_RAW = "../data/CCLE_Expression_Entrez_2012-09-29.gct"
+DRUG_RESPON_RAW = "../data/CCLE_NP24.2009_Drug_data_2015.02.24.csv"
 
 
 def main():
-    drug()
-    gene()
-
-
+    #drug()
+    #gene()
+    cell()
 
 
 def drug():
@@ -30,6 +31,28 @@ def gene():
     with open("data/first_model/gene.txt", "w") as f:
         for i, gene in enumerate(gene_set):
             f.write("G{0}\t{1}\n".format(i, gene))
+        f.close()
+
+
+def cell():
+    # intersection of cell lines from all data sources
+    df1 = pd.read_csv(ESSEN_RAW, delimiter="\t")
+    cell1 = set(df1.columns)
+    cell1.remove("Name")
+    cell1.remove("Description")
+    
+    df2 = pd.read_csv(MRNA_RAW, low_memory=False, delimiter="\t")
+    cell2 = set(df2.columns)
+    cell2.remove("Name")
+    cell2.remove("Description")
+
+    df3 = pd.read_csv(DRUG_RESPON_RAW)
+    cell3 = set(df3["CCLE Cell Line Name"])
+
+    cell_set = cell1.intersection(cell2).intersection(cell3)
+    with open("data/first_model/cell.txt", "w") as f:
+        for i, cell in enumerate(cell_set):
+            f.write("C{0}\t{1}\n".format(i, cell))
         f.close()
 
     
