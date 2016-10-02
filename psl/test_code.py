7d86a94b529b140e92d34b015ffb82ba97c2b56f
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-02 16:59:09 -0700

    add real test for percentile scaler

--- psl/test_code.py
+++ psl/test_code.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import generate_data
 
-def test_scaler():
+def test_percentile_scaler():
     df = pd.DataFrame(np.random.randint(0, 10, size=(4,4)), 
                       columns=list("ABCD"))
-    print df
-    assert True
+    df_percentile = generate_data.percentile_scaler(df)
+    for column in df.columns:
+        for idx in df.index:
+            assert df_percentile[column][idx] >= 0 and df_percentile[column][idx] <= 1
