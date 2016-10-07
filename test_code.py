7649907cd897e7f421dc6a668b6510e38c72f588
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-07 15:19:41 -0700

    add test for cross validation funciton

--- test_code.py
+++ test_code.py
@@ -2,10 +2,20 @@
 import pandas as pd
 import numpy as np
 import generate_data
+from evaluation import train_test_split
+
 
 def test_percentile_scaler():
     df = pd.DataFrame(map(float, range(10)), columns=list("A"))
     df_percentile = generate_data.percentile_scaler(df)
     for idx in df.index:
         assert df_percentile["A"][idx] == (idx + 1)/10.0
-        assert type(df_percentile["A"][idx]) == np.float64 
+        assert type(df_percentile["A"][idx]) == np.float64
+
+def test_cross_validation_split():
+    df = pd.DataFrame(range(20), columns=["A"])
+    for fold, train_df, val_df in train_test_split.cross_validation_split(df):
+        assert set(list(train_df.A) + list(val_df.A)) == set(df.A)
+    assert fold == 5 # check default 5 fold cv
+
+ 
