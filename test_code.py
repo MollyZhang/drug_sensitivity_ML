024809338c335720912bfb3b3e4396bd8f920226
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-10 20:04:23 -0700

    add more test for percentile scaler

--- test_code.py
+++ test_code.py
@@ -11,6 +11,18 @@ def test_percentile_scaler():
     for idx in df.index:
         assert df_percentile["A"][idx] == (idx + 1)/10.0
         assert type(df_percentile["A"][idx]) == np.float64
+    
+    df1 = pd.DataFrame(np.random.normal(size=1000))
+    df1_percentile = generate_data.percentile_scaler(df1)
+    df1_p = df1_percentile[0]
+    df1_p[df1_p >= 0.75] = 1
+    df1_p[df1_p <= 0.25] = 0
+    bottom25 = df1_p.value_counts().iloc[0]
+    top25 = df1_p.value_counts().iloc[1]    
+    assert bottom25 in [top25-1, top25, top25+1]
+    
+
+
 
 def test_cross_validation_split():
     df = pd.DataFrame(range(20), columns=["A"])
