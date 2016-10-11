e3731ea500d6e1c9440d04345d5e45f32d88872a
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-11 11:13:47 -0700

    add binarification of regression result and calculate accuracy from that

--- evaluation/compare_y.py
+++ evaluation/compare_y.py
@@ -14,7 +14,6 @@ def main():
     print calculate_accuracy(val_df, infer_df)
 
 
-
 def load_data(train_file, test_file, infer_file):
     dataframes = []
     for each_file in [train_file, test_file, infer_file]:
@@ -24,7 +23,7 @@ def load_data(train_file, test_file, infer_file):
                 items = line.strip("\n").split("\t")
                 cell_drug_pair = items[0] + items[1]
                 value = float(items[-1])
-                rows.append({"cell_drug_pair": cell_drug_pair, "sensitivity": value})
+                rows.append({"cell_drug_pair": cell_drug_pair, "y": value})
         f.close()
         df = pd.DataFrame(rows)
         dataframes.append(df)
@@ -33,11 +32,23 @@ def load_data(train_file, test_file, infer_file):
 
 def calculate_accuracy(truth_df, infer_df):
     infer_df = infer_df.loc[infer_df.cell_drug_pair.isin(truth_df.cell_drug_pair)].copy()
-    df = pd.merge(infer_df, truth_df, on="cell_drug_pair", suffixes=["_truth", "_infer"]) 
-    truth = np.array(df["sensitivity_truth"])
-    infer = np.array(df["sensitivity_infer"])
-    mse = sklearn.metrics.mean_squared_error(truth, infer)
-    return mse 
+    df = pd.merge(infer_df, truth_df, on="cell_drug_pair", suffixes=["_infer", "_truth"]) 
+    mse = sklearn.metrics.mean_squared_error(df.y_truth, df.y_infer)
+    accuracy, auc = binary_result(df)
+
+    return mse, accuracy, auc
+
+
+def binary_result(df):
+    """ take >75% as label 1 and <25% as label 0, return accuracy"""
+    df.y_truth[df.y_truth >= 0.75] = 1 
+    df.y_truth[df.y_truth <= 0.25] = 0
+    binary_df = df[(df.y_truth == 1) | (df.y_truth==0)].copy()
+    binary_df.y_infer[df.y_infer >= 0.5] = 1 
+    binary_df.y_infer[df.y_infer < 0.5] = 0
+    accuracy = sklearn.metrics.accuracy_score(binary_df.y_truth, binary_df.y_infer)
+    auc = sklearn.metrics.roc_auc_score(binary_df.y_truth, binary_df.y_infer)
+    return accuracy, auc 
 
 
 if __name__=="__main__":
