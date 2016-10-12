94e057e3ab20c6d84643d6d315326973af9607e5
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-12 13:20:02 -0700

    add plotting to weight_learning_curve.py

--- evaluation/weight_learning_curve.py
+++ evaluation/weight_learning_curve.py
@@ -1,10 +1,15 @@
 """parse weight updates from PSL debug log and plot the rule weight change over iterations
 """
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+
 
 def main():
-    weights = parse_weight("../psl/result/first_model_cross_val_fold1_log.txt")
-    print weights
-
+    weight_df = parse_weight("../psl/result/first_model_cross_val_fold1_log.txt")
+    plotting(weight_df)
 
 def parse_weight(log_file):
     rule_text = {"} ( DRUGTARGET(D, G) & ESSENTIAL(C, G) ) >> SENSITIVE(C, D)": "essential_rule",
@@ -20,7 +25,14 @@ def parse_weight(log_file):
         weights[rule] = [float(line.split(rule_text)[0].split("{")[-1])
                          for line in all_lines
                          if (rule_text in line and "kernel" in line)]
-    return weights
+    return pd.DataFrame(weights)
+
+
+def plotting(df):
+    df.plot()
+    plt.xlabel("Iteration")
+    plt.ylabel("Rule Weights")
+    plt.show()
 
 
 
