e5650c5e6a5a740b5ef6ba5df34e035c0b880f7e
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-19 10:11:31 -0700

    update AccuracyCurve.groovy to accomodate one set of weights at a time

--- psl/src/main/java/edu/ucsc/AccuracyCurve.groovy
+++ psl/src/main/java/edu/ucsc/AccuracyCurve.groovy
@@ -31,80 +31,70 @@ import edu.umd.cs.psl.util.database.Queries;
 import edu.umd.cs.psl.evaluation.statistics.*;
 
 
+essen_weight = Double.parseDouble(this.args[0]);
+active_weight = Double.parseDouble(this.args[1]);
+prior_weight = Double.parseDouble(this.args[2]);
 
-filename = this.args[0]
-File file = new File(filename)
-weight_list = file.text.split("\n")[1..-1]
+// psl initial setup
+ConfigManager cm = ConfigManager.getManager()
+ConfigBundle config = cm.getBundle("first-model")
+def defaultPath = System.getProperty("java.io.tmpdir")
+String dbpath = config.getString("dbpath", defaultPath + File.separator + "first-model")
+DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, true), config)
 
-weight_list.eachWithIndex { weights, index ->
-    println " " 
-    println "Iteration ${index}:"
-    // parse weight for this iteration
-    active_weight = Double.parseDouble(weights.split("\t")[0]);
-    essen_weight = Double.parseDouble(weights.split("\t")[1]);
-    prior_weight = Double.parseDouble(weights.split("\t")[2]);
+// define psl model 
+PSLModel m = new PSLModel(this, data)
+m.add predicate: "Drug",        types: [ArgumentType.UniqueID, ArgumentType.String]
+m.add predicate: "Gene",        types: [ArgumentType.UniqueID, ArgumentType.String]
+m.add predicate: "Cell",        types: [ArgumentType.UniqueID, ArgumentType.String]
+m.add predicate: "DrugTarget",  types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+m.add predicate: "Essential",   types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+m.add predicate: "Active",      types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+m.add predicate: "Sensitive",   types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
+m.add rule : ( DrugTarget(D, G) & Essential(C, G) ) >> Sensitive(C, D),  weight : essen_weight
+m.add rule : ( DrugTarget(D, G) & Active(C, G) ) >> Sensitive(C, D),  weight : active_weight
+m.add rule: ~Sensitive(C, D), weight: prior_weight 
 
-    // psl initial setup
-    ConfigManager cm = ConfigManager.getManager()
-    ConfigBundle config = cm.getBundle("first-model")
-    def defaultPath = System.getProperty("java.io.tmpdir")
-    String dbpath = config.getString("dbpath", defaultPath + File.separator + "first-model")
-    DataStore data = new RDBMSDataStore(new H2DatabaseDriver(Type.Disk, dbpath, true), config)
-   
-    // define psl model 
-    PSLModel m = new PSLModel(this, data)
-    m.add predicate: "Drug",        types: [ArgumentType.UniqueID, ArgumentType.String]
-    m.add predicate: "Gene",        types: [ArgumentType.UniqueID, ArgumentType.String]
-    m.add predicate: "Cell",        types: [ArgumentType.UniqueID, ArgumentType.String]
-    m.add predicate: "DrugTarget",  types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
-    m.add predicate: "Essential",   types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
-    m.add predicate: "Active",      types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
-    m.add predicate: "Sensitive",   types: [ArgumentType.UniqueID, ArgumentType.UniqueID]
-    m.add rule : ( DrugTarget(D, G) & Essential(C, G) ) >> Sensitive(C, D),  weight : essen_weight
-    m.add rule : ( DrugTarget(D, G) & Active(C, G) ) >> Sensitive(C, D),  weight : active_weight
-    m.add rule: ~Sensitive(C, D), weight: prior_weight 
-    
-    println m;
+println m;
 
-    // loads data
-    def dir = 'data'+java.io.File.separator +"first_model"+java.io.File.separator;
-    def target_dir = "seed0" + java.io.File.separator + "cross_val" + java.io.File.separator + "5fold" + java.io.File.separator;
-    def evidencePartition = new Partition(0);
-    
-    insert = data.getInserter(Drug, evidencePartition);
-    InserterUtils.loadDelimitedData(insert, dir+"drug.txt");
-    insert = data.getInserter(Gene, evidencePartition);
-    InserterUtils.loadDelimitedData(insert, dir+"gene.txt");
-    insert = data.getInserter(Cell, evidencePartition);
-    InserterUtils.loadDelimitedData(insert, dir+"cell.txt");
-    insert = data.getInserter(DrugTarget, evidencePartition);
-    InserterUtils.loadDelimitedData(insert, dir+"drug_target.txt");
-    insert = data.getInserter(Essential, evidencePartition);
-    InserterUtils.loadDelimitedDataTruth(insert, dir+"essential.txt");
-    insert = data.getInserter(Active, evidencePartition);
-    InserterUtils.loadDelimitedDataTruth(insert, dir+"active.txt");
-    
-    def targetPartition = new Partition(1);
-    insert = data.getInserter(Sensitive, targetPartition);
-    InserterUtils.loadDelimitedData(insert, dir + "sensitive_target.txt");
-    
-    Database db = data.getDatabase(targetPartition, [Drug, Gene, Cell, DrugTarget, Essential, Active] as Set, evidencePartition);
-    
-    // run inference
-    println "run inference"
-    MPEInference inferenceApp = new MPEInference(m, db, config);
-    inferenceApp.mpeInference();
-    inferenceApp.close();
-    
-    println "saving inference results"
-    DecimalFormat formatter = new DecimalFormat("#.#######");
-    def result_file = new File("result/log/weight_curve/${index}.txt");
-    result_file.write ""
-    for (GroundAtom atom : Queries.getAllAtoms(db, Sensitive)) {
-        for (int i=0; i<2; i++) {
-            result_file << atom.arguments[i].toString() + "\t"
-        }
-        result_file << formatter.format(atom.getValue()) + "\n"}
-    
-    db.close();
-}
+// loads data
+def dir = 'data'+java.io.File.separator +"first_model"+java.io.File.separator;
+def target_dir = "seed0" + java.io.File.separator + "cross_val" + java.io.File.separator + "5fold" + java.io.File.separator;
+def evidencePartition = new Partition(0);
+
+insert = data.getInserter(Drug, evidencePartition);
+InserterUtils.loadDelimitedData(insert, dir+"drug.txt");
+insert = data.getInserter(Gene, evidencePartition);
+InserterUtils.loadDelimitedData(insert, dir+"gene.txt");
+insert = data.getInserter(Cell, evidencePartition);
+InserterUtils.loadDelimitedData(insert, dir+"cell.txt");
+insert = data.getInserter(DrugTarget, evidencePartition);
+InserterUtils.loadDelimitedData(insert, dir+"drug_target.txt");
+insert = data.getInserter(Essential, evidencePartition);
+InserterUtils.loadDelimitedDataTruth(insert, dir+"essential.txt");
+insert = data.getInserter(Active, evidencePartition);
+InserterUtils.loadDelimitedDataTruth(insert, dir+"active.txt");
+
+def targetPartition = new Partition(1);
+insert = data.getInserter(Sensitive, targetPartition);
+InserterUtils.loadDelimitedData(insert, dir + "sensitive_target.txt");
+
+Database db = data.getDatabase(targetPartition, [Drug, Gene, Cell, DrugTarget, Essential, Active] as Set, evidencePartition);
+
+// run inference
+println "run inference"
+MPEInference inferenceApp = new MPEInference(m, db, config);
+inferenceApp.mpeInference();
+inferenceApp.close();
+
+println "saving inference results"
+DecimalFormat formatter = new DecimalFormat("#.#######");
+def result_file = new File(args[3]);
+result_file.write ""
+for (GroundAtom atom : Queries.getAllAtoms(db, Sensitive)) {
+    for (int i=0; i<2; i++) {
+        result_file << atom.arguments[i].toString() + "\t"
+    }
+    result_file << formatter.format(atom.getValue()) + "\n"}
+
+db.close();
