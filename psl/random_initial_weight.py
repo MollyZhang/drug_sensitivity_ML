a17ae0c0f4d376adc4400b50b4fdd61a3209e197
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-18 17:36:16 -0700

    update random initializaiton of weight experiment so that weights can be parsed directly from log output

--- psl/random_initial_weight.py
+++ psl/random_initial_weight.py
@@ -4,7 +4,7 @@ initialize rule weights as numbers between 0-1 and plot weight learning + accura
 
 import numpy as np
 import subprocess
-
+from evaluation import weight_learning_curve as wlc 
 
 
 def main():
@@ -14,9 +14,12 @@ def main():
 
     initial_weights = initialize_weights()
     for weights in initial_weights:
+        essen_w, active_w, prior_w = map(lambda x: str(x), weights)
         classpath = subprocess.check_output(["cat", "classpath.out"])
-        subprocess.call(["java", "-cp", "./target/classes:{0}".format(classpath),
-                         "edu.ucsc.ToyDrugExample"])
+        log = subprocess.check_output(["java", "-cp", "./target/classes:{0}".format(classpath),
+                         "edu.ucsc.ArgWeight", essen_w, active_w, prior_w])
+        df = wlc.parse_weight(log)
+        print df     
         break
 
 
