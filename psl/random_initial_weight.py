527a241563f81cbad6c6fbd6bebba318f65f45b0
Molly Zhang <mollyzhang@ucsc.edu>
2016-10-26 13:56:06 -0700

    seperate training and testing mse in random weight initialization experiemnts

--- psl/random_initial_weight.py
+++ psl/random_initial_weight.py
@@ -18,14 +18,16 @@ def main():
 
 
 def plot_result():
-    initial_weights = initialize_weights()
+    initial_weights = initialize_weights() + initialize_weights2()
     for weights in initial_weights:
         print weights
         folder = "./result/random_initial_weight/weights_" + "_".join(weights)
         df = pd.read_csv(folder + "/weight_change.csv", header=None, delimiter="\t", 
                          names=["essential_rule", "active_rule", "sensitive_prior"])
-        df['mse'] = wlc.get_accuracy_curve(folder, len(df)-1)
-        wlc.plotting(df, title=" ".join(weights), 
+        train_mse, test_mse = wlc.get_accuracy_curve(folder, len(df)-1)
+        df['train_mse'] = train_mse
+        df['test_mse'] = test_mse
+        wlc.plotting(df, title="initial weights: "+"_".join(weights), 
                      save_to="../plots/random_initial_weight/weights_{0}.png".format("_".join(weights)))
 
 
@@ -70,6 +72,19 @@ def initialize_weights():
                 w.append([str(a),str(b),str(c)])
 #    pprint.pprint(w)
     return w
+
+
+def initialize_weights2():
+    w = []
+    weight_range = range(1, 10)
+    for a in weight_range:
+        for b in weight_range:
+            for c in weight_range:
+                if a+b+c == 10:
+                    w.append(["0."+str(a), "0."+str(b), "0."+str(c)])
+    return w
+
+
 
 
 if __name__=="__main__":
